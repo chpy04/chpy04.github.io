57d59805ca43ef2930d92cd924e67e47f7616126
Chris Pyle <cpyle@unicode.org>
2025-06-12 07:23:39 -0400

    static github build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
 };
 
